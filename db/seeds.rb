e13b490cbe6b91e32f5be4d611521b2e50a32e91
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-22 17:00:57 -0500

    Create airport model
    Create model and seeding to populate default values.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(code: 'UIO')
+Airport.create(code: 'GYE')
+Airport.create(code: 'CUE')
+Airport.create(code: 'RIO')
+Airport.create(code: 'ATF')
