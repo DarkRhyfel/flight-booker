e4c9e251e66a3f8445dd52831482c2e6ea6cffdc
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-25 11:50:54 -0500

    Migrate and seed database.

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,6 +2,6 @@
 
 # Airport model
 class Airport < ApplicationRecord
-  has_many :departing_flights, class: 'Flights', foreign_key: 'departure_airport_id'
-  has_many :arriving_flights, class: 'Flights', foreign_key: 'arrival_airport_id'
+  has_many :departing_flights, class_name: 'Flights', foreign_key: 'departure_airport_id'
+  has_many :arriving_flights, class_name: 'Flights', foreign_key: 'arrival_airport_id'
 end
