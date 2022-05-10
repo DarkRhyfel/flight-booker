34871178e81cd46ee9b864d3727590f0c963b762
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-05-10 11:19:57 -0500

    Generate mailer templates
    Create mailer templates for booking confirmation.

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Mailer for passengers
 class PassengerMailer < ApplicationMailer
+  default from: 'el_mas_kapito@interairlines.com'
+
+  def booking_confirmation
+    @passenger = params[:passenger]
+    @flight = params[:flight]
+    mail(to: @passenger.email, subject: 'Booking confirmation')
+  end
 end
